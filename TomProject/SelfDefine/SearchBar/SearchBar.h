8b0ea84d30256e169ee7e7c441a0d6fdb24e7f6e
lck93 <101243955@qq.com>
2017-11-01 16:39:05 +0800

    searchbar

---
+++ TomProject/SelfDefine/SearchBar/SearchBar.h
@@ -0,0 +1,13 @@
+//
+//  SearchBar.h
+//  TomProject
+//
+//  Created by JDBeer on 2017/11/1.
+//  Copyright © 2017年 zhongHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchBar : UITextField
+
+@end
---
+++ TomProject/SelfDefine/SearchBar/SearchBar.m
@@ -0,0 +1,21 @@
+//
+//  SearchBar.m
+//  TomProject
+//
+//  Created by JDBeer on 2017/11/1.
+//  Copyright © 2017年 zhongHui. All rights reserved.
+//
+
+#import "SearchBar.h"
+
+@implementation SearchBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
