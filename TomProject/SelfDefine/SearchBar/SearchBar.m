a723fa13e472a44cbeb0fabad44c9dfb1360fc66
lck93 <101243955@qq.com>
2017-11-01 17:38:59 +0800

    searchbar

--- TomProject/SelfDefine/SearchBar/SearchBar.m
+++ TomProject/SelfDefine/SearchBar/SearchBar.m
@@ -10,12 +10,20 @@
 
 @implementation SearchBar
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+        UIImageView *leftView = [[UIImageView alloc] init];
+        leftView.image = ImageNamed(@"icon_search_shouye");
+        leftView.width = leftView.image.size.width+10;
+        leftView.height = leftView.image.size.height;
+        self.leftView = leftView;
+        self.leftViewMode = UITextFieldViewModeAlways;
+        self.placeholder = @"请输入商家";
+    }
+    return self;
 }
-*/
 
 @end
